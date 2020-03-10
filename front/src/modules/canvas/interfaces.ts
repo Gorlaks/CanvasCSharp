850ecfa1064b0dc120a6226e2f1e906d86e808e4
Gorlaks <imgorlaks@gmail.com>
2020-03-10 18:29:39 +0300

    Canvas-Front bug fix with removing

--- front/src/modules/canvas/interfaces.ts
+++ front/src/modules/canvas/interfaces.ts
@@ -7,5 +7,5 @@ export interface ICanvasRepository {
 export interface ICanvasService {
 		deleteCanvas(userId: string, canvasId: string): Promise<any>;
 		createCanvas(userId: string, title: string, type: string): Promise<any>;
-		setCanvasListAfterRemove(canvasId: string): void;
+		setCanvasListAfterRemoving(canvasId: string): void;
 }
