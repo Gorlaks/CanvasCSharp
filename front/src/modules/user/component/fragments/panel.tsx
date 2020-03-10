bf879f459fe627ef939a8a22840a5e11ffb48f6a
Gorlaks <imgorlaks@gmail.com>
2020-03-10 12:29:40 +0300

    Canvas-Front create modal canva

--- front/src/modules/user/component/fragments/panel.tsx
+++ front/src/modules/user/component/fragments/panel.tsx
@@ -3,10 +3,13 @@ import React from "react";
 import { LS } from "../../../../utils/helpers";
 import { PlusButton } from "../../../../assets/ui/ui";
 
-const Panel = () => {
+const Panel = (props: {
+	setCreateCanvasModalState: Function
+}) => {
+	const { setCreateCanvasModalState } = props;
 	return (
 		<div className="user__panel">
-			<PlusButton text={LS("Create")} handleClick={() => {}} />
+			<PlusButton text={LS("Create")} handleClick={() => setCreateCanvasModalState(true)} />
 		</div>
 	)
 }
--- front/src/modules/user/component/user.tsx
+++ front/src/modules/user/component/user.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { connect } from "react-redux";
 
 import { LS } from "../../../utils/helpers";
@@ -6,19 +6,26 @@ import { IReduxStore, IUserAuthData, ICanvasList } from "../../common/redux/inte
 
 import Panel from "./fragments/panel";
 import Table from "./fragments/table";
+import CreateCanvasModal from "../../createCanvasModal/createCanvasModal";
 
 const User = (props: {
 	language: string,
 	userAuthData: IUserAuthData,
 	canvasList: Array<ICanvasList>
 }) => {
+	const [createCanvasModalIsOpened, setCreateCanvasModalState] = useState(false);
+
 	return (
 		<div className="user">
 			<p className="user__title">{LS("Canvas_list")}</p>
-			<Panel />
+			<Panel setCreateCanvasModalState={setCreateCanvasModalState} />
 			<Table
 				userAuthData={props.userAuthData}
 				canvasList={props.canvasList}
+			/>
+			<CreateCanvasModal
+				isOpened={createCanvasModalIsOpened}
+				setModalState={setCreateCanvasModalState}
 			/>
 		</div>
 	)
